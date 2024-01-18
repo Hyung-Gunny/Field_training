5e201088aa3c142455d5806e8fa6f2ddfda81b37
Hyung-Gunny <podot3059@naver.com>
2024-01-18 17:24:05 +0900

    2024.1.18

--- src/main/java/com/example/demo/login/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.login.repository;
-
-import com.example.demo.login.dto.SiteUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
-}
