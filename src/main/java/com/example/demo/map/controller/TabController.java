f00e64b7443274552cabe8e8c834dd28cbc74981
Hyung-Gunny <podot3059@naver.com>
2024-02-07 17:39:12 +0900

    2024.2.07

--- src/main/java/com/example/demo/map/controller/TabController.java
+++ src/main/java/com/example/demo/map/controller/TabController.java
@@ -48,4 +48,6 @@ public class TabController
         return "collaboration";
     }
 
+    @GetMapping("/integratedpipeline")
+    public String Integratedpipeline(){return "integratedpipeline";}
 }
